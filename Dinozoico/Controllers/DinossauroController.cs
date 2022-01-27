130f0b951f285175ce32ce19481dbcc064131f8c
yorhanSD <yorhansd08@gmail.com>
2022-01-27 17:51:49 -0300

    Initial commit

---
+++ Dinozoico/Controllers/DinossauroController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dinozoico.Controllers
+{
+    public class DinossauroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult DeletarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
